7eeb0bc82f4a262de629647993f5ba9333401fae
DeveloperTouseeq <developertouseeq@gmail.com>
2024-05-28 23:31:25 +0500

    get section data from api

--- lib/presentarion/sections_screen/controller/sectioons_controller.dart
+++ lib/presentarion/sections_screen/controller/sectioons_controller.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:hajjiapp/presentarion/sections_screen/models/sections_model.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 import '../../../Shared_prefrences/app_prefrences.dart';
@@ -15,6 +17,8 @@ import '../../../core/utils/utils.dart';
 import '../../../data/services/api_call_status.dart';
 import '../../../data/services/api_exceptions.dart';
 import '../../../data/services/base_client.dart';
+import '../../../localization/strings_enum.dart';
+import '../../../widgets/custom_snackbar.dart';
 import '../../../widgets/paginations/paged_view.dart';
 
 /// A controller class for the DiscoverScreen.
@@ -24,6 +28,8 @@ import '../../../widgets/paginations/paged_view.dart';
 ///
 class SectionsController extends GetxController {
   final RoundedLoadingButtonController btnController = RoundedLoadingButtonController();
+  ApiCallStatus apiCallStatus = ApiCallStatus.holding;
+
  // Rx<DiscoverModel> discoverModelObj = DiscoverModel().obs;
 
   // LoginModel? userDetails;
@@ -35,9 +41,57 @@ class SectionsController extends GetxController {
   //   print('map $userMap');
   //   userDetails = LoginModel.fromJson(userMap);
   // }
+  final sectionData = <DataItem>[].obs;
 
 
+  Future<void> getSectionData() async {
 
+      Utils.check().then((value) async {
+        if (value) {
+          await BaseClient.post(
+              Constants.sectionDataUrl,
+              onSuccess: (response) async {
+
+                apiCallStatus = ApiCallStatus.success;
+
+                if (response.data['data'] != null) {
+
+                  // Map<String, dynamic> parsedJson = jsonDecode(jsonData);
+
+                  sectionData.value= List<DataItem>.from(response.data['data'].map((x) => DataItem.fromJson(x)));
+
+                  // Now you can access the parsed data
+                  print(sectionData.value[0].sectionNumber); // Output: 1
+                  print(sectionData.value[0].type); // Output: Female
+                  Utils.showToast(response.data['message'], false,);
+                  print("masg ${response.data['message']}");
+                } else {
+                  Utils.showToast('', true);
+                  // Handle the case where data is null in the response
+                }
+              },
+              onError: (error) {
+                BaseClient.handleApiError(error);
+                apiCallStatus = ApiCallStatus.error;
+              },
+
+
+              data: {
+                {
+                  "user_id": "778",
+                  "session_code": "b6b845e9d24faffae128"
+                }
+              }
+          );
+
+        } else {
+          CustomSnackBar.showCustomErrorToast(
+            message: Strings.noInternetConnection.tr,
+          );
+        }
+      });
+
+  }
 
 
 
@@ -46,7 +100,7 @@ class SectionsController extends GetxController {
   void onInit() {
     super.onInit();
 
-
+getSectionData();
 
   }
 
--- lib/presentarion/sections_screen/models/sections_model.dart
+++ lib/presentarion/sections_screen/models/sections_model.dart
@@ -11,3 +11,19 @@ class Bed {
     this.hasAC,
   });
 }
+class DataItem {
+  String sectionNumber;
+  String type;
+
+  DataItem({
+    required this.sectionNumber,
+    required this.type,
+  });
+
+  factory DataItem.fromJson(Map<String, dynamic> json) {
+    return DataItem(
+      sectionNumber: json['section_number'],
+      type: json['type'],
+    );
+  }
+}
