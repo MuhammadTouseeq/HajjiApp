54a6c69703ac58e92fb8919d6b8410b00e9b659e
DeveloperTouseeq <developertouseeq@gmail.com>
2024-06-13 01:47:05 +0500

    firebase setup and push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,19 +10,39 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:hajjiapp/routes/app_routes.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+import 'core/utils/fcm_helper.dart';
 import 'core/utils/initial_bindings.dart';
 import 'core/utils/logger.dart';
 import 'core/utils/size_utils.dart';
+import 'data/services/my_shared_pref.dart';
+import 'firebase_options.dart';
 import 'localization/app_localization.dart';
 
 void main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light,);
   WidgetsFlutterBinding.ensureInitialized();
+
+
+  // await Firebase.initializeApp();
+
+  Platform.isIOS?await Firebase.initializeApp(
+    name: 'hajji_app',
+    options: DefaultFirebaseOptions.ios,
+  ):await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+  // init shared preference
+  await MySharedPref.init();
+
+  // inti fcm & notifications services (awesome notifications)
+  await FcmHelper.initFcm();
+
   await Permission.location.request();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]).then((value) {
     Logger.init(kReleaseMode ? LogMode.live : LogMode.debug);
+
     runApp(MyApp());
   });
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
 import connectivity_plus
 import file_selector_macos
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
+import firebase_messaging
 import geolocator_apple
 import package_info
 import package_info_plus
@@ -17,8 +22,13 @@ import url_launcher_macos
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
